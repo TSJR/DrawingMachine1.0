b7d85c834c320753710cc7be6e2be76883756038
TSJR <42743941+TSJR@users.noreply.github.com>
2023-05-26 22:24:22 -0400

    Added comments

--- static/gen_coords.js
+++ static/gen_coords.js
@@ -10,6 +10,7 @@ export class Pixel {
 export function to_img(img, len) {
 
 }
+// Convert image to black and white
 export function get_bw() {
     let c = document.querySelector("canvas");
     let ctx = c.getContext("2d");
@@ -24,6 +25,8 @@ export function get_bw() {
             obj_data.push(cur_row);
             cur_row = [];
         }
+        
+        // Calculating brightness
         let brightness = 0;
         brightness += raw_data[i] / 255 * 0.21;
         brightness += raw_data[i + 1] / 255 * 0.72;
@@ -56,6 +59,7 @@ let color_map = {
     "0": 0
 };
 
+// Mapping brightness between 0 and 10
 function get_mapped(b) {
     let brightness = b[0];
     for (let i = 10; i > 0; i--) {
@@ -74,6 +78,7 @@ function get_mapped(b) {
 
 }
 
+// Is in function (standard JS func doesn't work for 2d arrays)
 function is_in(arr, item) {
     for (let i = 0; i < arr.length; i++) {
         if (item[0] === arr[i][0] && item[1] === arr[i][1]) {
@@ -83,6 +88,7 @@ function is_in(arr, item) {
     return false;
 }
 
+// Calculates adjacent pixels of same brightness
 function get_adjacent(pixels, checked, body, brightness, pixel) {
     let to_return = [];
     let new_checked = [];
@@ -93,15 +99,12 @@ function get_adjacent(pixels, checked, body, brightness, pixel) {
         let pos = positions[i];
 
         try {
-            //console.log(`${get_mapped(pixels[row + pos[0]][col + pos[1]])} to ${brightness}`);
             if (get_mapped(pixels[row + pos[0]][col + pos[1]]) === brightness) {
-                // console.log("valid");
                 new_checked.push([[row + pos[0]], [col + pos[1]]]);
                 to_return.push([[row + pos[0]], [col + pos[1]]]);
             }
         } catch {
-            //console.log([row, col]);
-            //console.log([pixels.length, pixels[0].length]);
+        
         }
 
     }
@@ -109,6 +112,7 @@ function get_adjacent(pixels, checked, body, brightness, pixel) {
 
 }
 function update_bar(size, i) {
+    // Progress bar (does not fully work)
     progress_bar.style.width = `${Math.round(i / size * 100)}%`;
     progress_counter.innerText = `${Math.round(i / size * 100)}%`;
     if (i === size) {
@@ -128,54 +132,50 @@ export async function get_bodies(obj_data) {
 
     for (let row = 0; row < obj_data.length; row++) {
         for (let col = 0; col < obj_data[0].length; col++) {
-
-            //console.log(checked.length);
+            
+            // Only continue if it is not in array already
             if (!is_in(checked, [row, col])) {
                 i++;
                 let brightness = get_mapped(obj_data[row][col]);
-                //console.log(`brightness is ${(obj_data[row][col])}`);
                 cur_body.push([row, col, brightness]);
-                //NOT A NEW PIXEL
                 let raw = get_adjacent(obj_data, checked, cur_body, brightness, [row, col]);
                 let adjacents = raw[0];
                 checked = checked.concat(raw[1]);
-                //console.log(raw[1].length);
                 to_check = to_check.concat(adjacents);
 
+                // Updating bar occasionally
                 if (i % 100 === 0 || i === size) {
                     update_bar(size, i);
                     await sleep(20);
                 }
 
+                // Keep going while there are more pixels to be checked per body
                 while (to_check.length !== 0) {
                     let next_check = [];
                     for (let i = 0; i < to_check.length; i++) {
                         let check = to_check[i];
-                        //NOT A NEW PIXEL
                         cur_body.push([check[0][0], check[1][0], brightness]);
                         let raw = get_adjacent(obj_data, checked, cur_body, brightness, [check[0], check[1]]);
                         next_check = next_check.concat(raw[0]);
                         try {
                             checked = checked.concat(raw[1])
                         } catch (e) {
-                            //console.log(e);
 
                        }
 
                     }
                     to_check = next_check;
-
-
                 }
 
                 bodies.push(cur_body);
                 cur_body = [];
 
             } else {
-                console.log("already there");
+
             }
         }
     }
     console.log(bodies.length);
+    // Returning each body as a collection of pixels
     return bodies;
 }
